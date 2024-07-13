c5fa1af03def8582895dae7253f5e039a970a916
Neno Arisma <nenoarisma@gmail.com>
2024-07-13 20:36:02 +0700

    feat: add which key plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -97,4 +97,22 @@ return {
     event = { "VeryLazy" },
     config = true,
   },
+
+  -- Whickey Plugin
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function() require("which-key").show { global = false } end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 }
