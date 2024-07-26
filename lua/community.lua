f05129a199718f3df88a6b3ed4eb68594a7a9044
Neno Arisma <nenoarisma@gmail.com>
2024-07-26 22:15:03 +0700

    feat: add nix plugin community

--- lua/community.lua
+++ lua/community.lua
@@ -17,6 +17,7 @@ return {
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.laravel" },
+  { import = "astrocommunity.pack.nix" },
   -- media pack
   { import = "astrocommunity.media.codesnap-nvim" },
   -- import/override with your plugins folder
